b717898559721e819c35da5bb3143fe8672034ff
agan2021 <ninghongquan@fjbu.edu.cn>
2023-03-18 15:51:12 +0800

    second

---
+++ .history/python/code/test_20230318154711.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+'''
+@File    :   test.py
+@Time    :   2023/03/18 15:45:29
+@Author  :   hong 
+@Version :   python3.8.12
+@Contact :   ninghongquan@163.com
+@Desc    :   None
+'''
+#TO:Make a progress everyday and get closer to the dream.
+
+import numpy as np 
+import pandas as pd
+import matplotlib.pyplot as plt
+x=np.linspace(0,10,1000)
+y=np.sin(x)
+plt.plot(x,y)
+plt.show()
